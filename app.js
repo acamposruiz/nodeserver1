5346f78f1a400693286a1240d417f7e937119f0c
root <root@d1stkfactory>
2015-02-15 09:37:38 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Module dependencies. */
+
+var express = require('express')
+  , http = require('http');
+
+var app = express(); 
+var server = http.createServer(app);
+
+app.get('/', function(req, res){
+  res.send('Hello World!!');
+});
+
+app.listen(3000);
