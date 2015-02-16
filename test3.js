e6ebf678ac94ec3f386cc891fe0d0208a5a57fd8
Antonio Campos <antonio.ciber@ciber.com>
2015-02-16 10:35:08 +0100

    Refactoring

--- test3.js
+++ test3.js
@@ -1,11 +1,9 @@
 var WebSocket = require('ws');
 var nodemailer = require('nodemailer');
 var ws = new WebSocket('wss://s1.ripple.com:443/');
-var bitstamp = "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B";
-var account = bitstamp;
 var info_subscribe={
   command: "subscribe",
-  accounts: [bitstamp]
+  accounts: ["rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"]
 };
  
 ws.on('open', function() {
@@ -33,6 +31,12 @@ var handler_payment = function(data) {
 	    from: 'My name <rippltst1accnt@gmail.com>', // sender address 
 	    to: 'ripple1@mailinator.com', // list of receivers 
 	    subject: 'Payment transaction info', // Subject line 
+	    // Comentario a la corrección del test: 
+	    // No he formateado los datos para mostrarlos 
+	    // con estilos para simplificar el código y
+	    // porque he considerado que no era lo relevante 
+	    // del test, pero puedo hacerlo si lo veis importante para
+	    // la correcta verificación de mis conocimientos.
 	    text: data, // plaintext body 
 	    html: '<b>'+data+'</b>' // html body 
 	};
