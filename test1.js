0be7ac59eef9b25696249fc9b4d70fba6bca8ec1
Antonio Campos <antonio.ciber@ciber.com>
2015-02-17 07:40:09 +0100

    Update test 1

--- test1.js
+++ test1.js
@@ -2,18 +2,18 @@ var WebSocket = require('ws');
 var ws = new WebSocket('wss://s1.ripple.com:443/');
 var info_subscribe={
   command: "subscribe",
-  accounts: ["rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B_data_error"]
+  accounts: ["rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B_force_error"]
 };
  
 ws.on('open', function() {
-    ws.send(JSON.stringify(info_subscribe), function(error) {
- 		ws.send(JSON.stringify({
- 		  command: "subscribe",
- 		  accounts: ["rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B"]
- 		}));
-	});
+    ws.send(JSON.stringify(info_subscribe));
 });
  
 ws.on('message', function(data, flags) {
     console.log(data);
+    if (JSON.parse(data).error != null) {
+    	console.log('Error in the connection: ' + JSON.parse(data).error);
+    	console.log('Resend connection');
+    	ws.send(JSON.stringify(info_subscribe));
+    };
 });
